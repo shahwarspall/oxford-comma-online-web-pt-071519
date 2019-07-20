a9d5a60d2f94f81cd214bfcb027df497c2d66df4
Shahwar Spall <shevy.spall@gmail.com>
2019-07-20 03:41:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,10 @@
 def oxford_comma(array)
   if array.length < 2 
     array.join
-  elsif array
-    
-end
+    elsif array.length == 2 
+    array.insert(1, "and").join(" ")
+  else 
+    str = ", and " + array.pop
+    str = array.join(", ") << str
+  end
 end
